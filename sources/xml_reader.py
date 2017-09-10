893bce4f8061c86eabf0cd904ceea614824f4e4b
Benoit Bourdon <chinchifou@gmail.com>
2017-09-10 20:53:48 +0200

    Use python to validate XML file
    + use lxml module for validation
    + update documentation to install lxml

---
+++ sources/xml_reader.py
@@ -0,0 +1,22 @@
+#~~~~~~ IMPORTS ~~~~~~
+
+#------ Standard library imports ------
+from os import path
+from lxml import etree
+
+
+#~~~~~~ INITIALIZATION ~~~~~~
+
+#------ Paths for files ------
+path_for_xml_file = path.join(path.abspath('../savegames/'),'template_save_game.xml')
+path_for_xsd_file = path.join(path.abspath('../savegames/'),'template_save_game.xsd')
+
+#------ Parsing files ------
+xml_tree = etree.parse(path_for_xml_file)
+xsd_tree = etree.parse(path_for_xsd_file)
+
+#~~~~~~ SCHEMA VALIDATION ~~~~~~
+xsd_schema = etree.XMLSchema(xsd_tree)
+validation_result = xsd_schema.validate(xml_tree)
+
+print(validation_result)
